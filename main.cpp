95a1fa98bbcf6c2a17629373f70c246365c8f607
ora amir <ora.amir@live.biu.ac.il>
2021-06-27 14:36:06 +0300

    changes to main and how to run the function testAlgorithm

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
-#include <unordered_map>
 #include "Algorithm.h"
 #include "AlgorithmInfAb.h"
 #include "Naive.h"
@@ -14,22 +12,31 @@ using namespace std;
 using namespace std::chrono;
 
 void analysisFileColumnNames(const string& fileName);
-void testAlgorithm(const string& textPath, const string& patternPath, int size_ab, const string& analysisFileName, const string& matchPosFileName);
+void testLengthPattern(const string& textPath, const string& patternPath, int size_ab, const string& analysisFileName,
+                       const string& matchPosFileName);
 void copyPosP_matchToFile(const vector<size_t>& matches, string fileName);
 void testManyTexts(const string& textNameStart, const string& analysisFileName, const string& matchPosFileNameStart);
+void testAlgorithm(vector<size_t> text, const string& textName, vector<size_t> pattern, const string& patternName,
+                   int size_ab, const string& analysisFileName, const string& matchPosFileName, size_t lengthPattern);
 
 
 
 int main(int argc, char** argv) {
     //arguments: 1-path to texts, 2-path to pattern, 3-name of match-Pos file, 4-name of analysis file, 5-size of ab
+    //add names to the columns in the analysis file
+    analysisFileColumnNames(argv[4]);
     //get all the text files in the folder and send them to the algorithm with the pattern
     for (const auto & entry : fs::directory_iterator(argv[1])) {
         if (fs::is_regular_file(entry.path())) { //not a directory. Is a text file
-            vector<unsigned int> text = readNumberFile(entry.path().string());
+            vector<size_t> text = readNumberFile(entry.path().string());
+            vector<size_t> pattern = readNumberFile(argv[2]);
             string textName = entry.path().filename().string();
             string matchPosFilename = argv[3] + textName;
             //send to algorithm
-            testAlgorithm(entry.path().string(), argv[2], stoi(argv[5]), argv[4], matchPosFilename);
+            fs::path pathToPattern = argv[2];
+            string patternName = pathToPattern.filename().string();
+            testAlgorithm(text, textName, pattern, patternName, stoi(argv[5]),
+                          argv[4], matchPosFilename, pattern.size());
         }
     }
     return 0;
@@ -40,53 +47,59 @@ int main(int argc, char** argv) {
  * the function runs the naive algorithm and the algorithm, tests the time of each and writes the information to the
  * analysis file
  */
-void testAlgorithm(const string& textPath, const string& patternPath, int size_ab, const string& analysisFileName,
-                   const string& matchPosFileName) {
+void testLengthPattern(const string& textPath, const string& patternPath, int size_ab, const string& analysisFileName,
+                       const string& matchPosFileName) {
     vector<size_t> text = readNumberFile(textPath);
     vector<size_t> pattern = readNumberFile(patternPath);
 
     //for every text file test length of pattern in different sizes
     for (int k = 16; k <= pattern.size(); k*=2) {
-        //naive algorithm
-        vector<size_t> matchesNaive;
-        matchesNaive.reserve(text.size());
-        auto start = high_resolution_clock::now(); // Get starting timepoint
-        naive(matchesNaive, text.data(), text.size(), pattern.data(), k, size_ab);
-        auto stop = high_resolution_clock::now(); // Get ending timepoint
-        auto durationNaive = duration_cast<microseconds>(stop - start);
-
-        //algorithm
-        vector<size_t> matchesAlgorithm;
-        matchesAlgorithm.reserve(text.size());
-        start = high_resolution_clock::now(); // Get starting timepoint
-        Algorithm *algorithm1 = new Algorithm(text.data(), text.size(), pattern.data(), k, size_ab);
-        algorithm1->runAlgorithm(matchesAlgorithm);
-        stop = high_resolution_clock::now(); // Get ending timepoint
-        auto durationAlgorithm = duration_cast<microseconds>(stop - start);
-
-        //time of each algorithm
-        auto naiveTime = durationNaive.count();
-        auto algoTime = durationAlgorithm.count();
-
         fs::path pathToText = textPath;
         fs::path pathToPattern = patternPath;
         string textName = pathToText.filename().string();
         string patternName = pathToPattern.filename().string();
-        string matchPosFileNameFull = matchPosFileName+ "PatternLen"+ to_string(k) + ".txt";
-
-        fstream fout;
-        //add info to the file
-        fout.open(analysisFileName, ios::out | ios::app);
-        //columns:Name of text file, Name of pattern file, Length of text, Length of pattern, Size of alphabet,
-        //      Time taken by naive, Time taken by algorithm, How much faster is the algorithm, Number of matchesAlgorithm,
-        //      File with matchesAlgorithm
-        fout <<textName+"," <<patternName+"," << text.size() << "," << k << "," << size_ab << "," << naiveTime << ","
-             << algoTime << "," << (static_cast<float>(naiveTime) / algoTime) << "," << matchesAlgorithm.size() << "," << matchPosFileNameFull << "\n";
-        fout.close();
-
-        //write list of p-matchesAlgorithm to a file
-        copyPosP_matchToFile(matchesAlgorithm, matchPosFileNameFull);
+        testAlgorithm(text, textName, pattern, patternName, size_ab, analysisFileName, matchPosFileName, k);
     }
+}
+
+void testAlgorithm(vector<size_t> text, const string& textName, vector<size_t> pattern, const string& patternName, int size_ab, const string& analysisFileName,
+                   const string& matchPosFileName, size_t lengthPattern) {
+    vector<size_t> matchesNaive;
+    matchesNaive.reserve(text.size());
+    auto start = high_resolution_clock::now(); // Get starting timepoint
+    naive(matchesNaive, text.data(), text.size(), pattern.data(), lengthPattern, size_ab);
+    auto stop = high_resolution_clock::now(); // Get ending timepoint
+    auto durationNaive = duration_cast<microseconds>(stop - start);
+
+    //algorithm
+    vector<size_t> matchesAlgorithm;
+    matchesAlgorithm.reserve(text.size());
+    start = high_resolution_clock::now(); // Get starting timepoint
+    Algorithm *algorithm1 = new Algorithm(text.data(), text.size(), pattern.data(), lengthPattern, size_ab);
+    algorithm1->runAlgorithm(matchesAlgorithm);
+    stop = high_resolution_clock::now(); // Get ending timepoint
+    auto durationAlgorithm = duration_cast<microseconds>(stop - start);
+
+    //time of each algorithm
+    auto naiveTime = durationNaive.count();
+    auto algoTime = durationAlgorithm.count();
+
+    string matchPosFileNameFull = matchPosFileName+ "PatternLen"+ to_string(lengthPattern) + ".txt";
+    fs::path pathTomatchPosFile = matchPosFileNameFull;
+    string matchPosFile = pathTomatchPosFile.filename().string();
+    fstream fout;
+    //add info to the file
+    fout.open(analysisFileName, ios::out | ios::app);
+    //columns:Name of text file, Name of pattern file, Length of text, Length of pattern, Size of alphabet,
+    //      Time taken by naive, Time taken by algorithm, How much faster is the algorithm, Number of matchesAlgorithm,
+    //      File with matchesAlgorithm
+    fout <<textName+"," <<patternName+"," << text.size() << "," << lengthPattern << "," << size_ab << "," << naiveTime << ","
+         << algoTime << "," << (static_cast<float>(naiveTime) / algoTime) << "," << matchesAlgorithm.size() << "," << matchPosFile << "\n";
+    fout.close();
+
+    //write list of p-matchesAlgorithm to a file
+    copyPosP_matchToFile(matchesAlgorithm, matchPosFileNameFull);
+
 }
 
 /*
@@ -97,7 +110,7 @@ void testManyTexts(const string& textNameStart, const string& analysisFileName, 
         string textName = "files/" + textNameStart + to_string(size_ab) + ".txt";
         string patternName = "files/pattern" + to_string(size_ab) + ".txt";
         string matchPosFileName = matchPosFileNameStart+ "Ab"+ to_string(size_ab);
-        testAlgorithm(textName, patternName, size_ab, analysisFileName, matchPosFileName);
+        testLengthPattern(textName, patternName, size_ab, analysisFileName, matchPosFileName);
     }
 }
 
@@ -121,6 +134,7 @@ void analysisFileColumnNames(const string& fileName) {
     fstream fout;
     fout.open(fileName, ios::out | ios::app);
     fout<<"Name of text file,"<<"Name of pattern file,"<<"Length of text,"<<"Length of pattern,"<<"Size of alphabet,"
-    <<"Time taken by naive,"<<"Time taken by algorithm,"<<"How much faster is the algorithm,"<<"Number of matches,"<<"File with matches\n";
+    <<"Time taken by naive,"<<"Time taken by algorithm,"<<"How much faster is the algorithm,"<<"Number of matches,"<<
+    "File with matches\n";
     fout.close();
 }
